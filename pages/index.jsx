9ed34eb29af5e1437147ad3be59dc65c61b6a1ba
sigaeasu <sigikemahduta@gmail.com>
2022-04-28 14:18:54 +0700

    linking page using next/link

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,7 +1,22 @@
+import {Fragment} from 'react';
+import Link from 'next/link';
+
 export default function Home() {
   return (
-    <>
+    <Fragment>
       <h1>Home Page</h1>
-    </>
+      <ul>
+        <li>
+          <Link href="/news/krakatau-volcano-is-erupted">
+            Krakatau Volcano is Erupted
+          </Link>
+        </li>
+        <li>
+          <Link href="/news/earthquake-in-lombok">
+            Earthquake in Lombok
+          </Link>
+        </li>
+      </ul>
+    </Fragment>
   )
 }
